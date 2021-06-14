5b3a18bc891c74bd885f532485f22f2fb85156de
vincentaulia <vincentaulia@gmail.com>
2021-06-13 21:03:03 -0400

    Add project files.

---
+++ Personal.SeleniumWebDriver/TestMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Personal.SeleniumWebDriver
+{
+    public class TestMethods
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            }
+        }
+    }
+}
