5e421716a6776536101539f80fe80b33ff48f2bd
vincentaulia <vincentaulia@gmail.com>
2021-06-15 05:36:33 -0400

    added several more smoke tests

---
+++ Personal.SeleniumWebDriver/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace Personal.SeleniumWebDriver
+{
+    internal static class Helpers
+    {
+        //Brief delay to show browser interactions
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
--- Personal.SeleniumWebDriver/TestMethods.cs
+++ Personal.SeleniumWebDriver/TestMethods.cs
@@ -7,13 +7,83 @@ namespace Personal.SeleniumWebDriver
 {
     public class TestMethods
     {
+        const string homeUrl = "https://ultimateqa.com/automation";
+        const string aboutUrl = "https://ultimateqa.com/about/";
+        const string homeTitle = "Automation Practice - Ultimate QA";
+
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+                driver.Navigate().GoToUrl(homeUrl);
+
+                Helpers.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+
+                Helpers.Pause();
+
+                driver.Navigate().Refresh();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+                Helpers.Pause();
+
+                driver.Navigate().GoToUrl(aboutUrl);
+                Helpers.Pause();
+
+                driver.Navigate().Back();
+                Helpers.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnForward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(aboutUrl);
+                Helpers.Pause();
+
+                driver.Navigate().GoToUrl(homeUrl);
+                Helpers.Pause();
+                
+                driver.Navigate().Back();
+                Helpers.Pause();
+
+                driver.Navigate().Forward();
+                Helpers.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
             }
         }
     }
